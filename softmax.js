361e6612fc8471f51357dbd0c4d9c3c8cf34f65e
jordan.wang <jordan.wang@zoom.us>
2022-11-25 11:30:29 +0800

    feat: add softmax

---
+++ softmax.js
@@ -0,0 +1,14 @@
+function getSoftmax(inputs, correct_index) {
+  const sum = inputs.reduce((acc, x) => {
+    return acc + Math.exp(x);
+  }, 0);
+  return inputs.map((x, index) => {
+    if (index === correct_index) {
+      return 1 - Math.exp(x) / sum;
+    } else {
+      return -Math.exp(x) / sum;
+    }
+  });
+}
+
+console.log(getSoftmax([1.3, 2.4, 3.7], 2));
